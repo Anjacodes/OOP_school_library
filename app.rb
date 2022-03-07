055a2d831837a084da4713292f64b0b23517946f
Anjacodes <anja_schmidt7@posteo.de>
2022-03-07 13:37:44 -0600

    Refactor create_rental

--- app.rb
+++ app.rb
@@ -27,25 +27,6 @@ class App
     end
   end
 
-  def create_rental
-    p 'Select a book from the following list by number'
-    @book_list.each_with_index do |book, index|
-      p "#{index}) Title: '#{book.title}', Author: #{book.author}"
-    end
-    book = @book_list[gets.chomp.to_i]
-    p 'Select a person from the following list by number (not id)'
-    @people_list.each_with_index do |person, index|
-      p "#{index}) [#{person.class.name}] Name: '#{person.name}', ID: #{person.id}, Age: #{person.age}"
-    end
-    person = @people_list[gets.chomp.to_i]
-    print 'Date: '
-    date = gets.chomp
-    @rental_list << Rental.new(date, book, person)
-    p 'Rental created successfully'
-    puts ''
-    run
-  end
-
   def rentals_for_id
     if @people_list.empty?
       p 'There is no one to choose from'
--- execute.rb
+++ execute.rb
@@ -3,6 +3,7 @@ require './app'
 require './create_person'
 require './list_books'
 require './create_book'
+require './create_rental'
 
 class Execute
   def initialize(state)
@@ -11,6 +12,7 @@ class Execute
     @person = Create_Person.new(@state)
     @list_books = List_Books.new(@state)
     @create_book = Create_Book.new(@state)
+    @create_rental = Create_Rental.new(@state)
   end
   def execute(user_choice) # rubocop:disable Metrics/CyclomaticComplexity
     case user_choice.to_i
@@ -18,7 +20,7 @@ class Execute
     when 2 then @people.list_people
     when 3 then @person.create_person
     when 4 then @create_book.create_book
-    when 5 then create_rental
+    when 5 then @create_rental.create_rental
     when 6 then rentals_for_id
     when 7 then exit_app
     else p 'Please enter a number between 1 and 7'
