3cdb9ea52d757b51cacf915ff5bb2a539c0986f3
Diego Solis <disc3110@gmail.com>
2022-03-08 09:23:03 -0600

    Refactor create book and create person inputs

--- create_book.rb
+++ create_book.rb
@@ -4,11 +4,16 @@ class CreateBook
     @book_list = @state[:book_list]
   end
 
-  def create_book
+  def get_book_details
     print 'Title: '
     title = gets.chomp
     print 'Author: '
     author = gets.chomp
+    [title, author]
+  end
+
+  def create_book
+    title, author = get_book_details
     @book_list << Book.new(title, author)
     p 'Book created successfully'
     puts ''
--- create_person.rb
+++ create_person.rb
@@ -3,33 +3,41 @@ class CreatePerson
     @state = state
   end
 
-  def create_person
+  def get_person_details
     print 'Do you want to create a student (1)  or a teacher (2)? [Input the number]: '
     user_choice = gets.chomp
     print 'Age: '
-    @age = gets.chomp.to_i
+    age = gets.chomp.to_i
     print 'Name: '
-    @name = gets.chomp
-    case user_choice.to_i
-    when 1 then create_student
-    when 2 then create_teacher
+    name = gets.chomp
+    if user_choice.to_i == 1
+      print 'Has parent permission? [Y/N]: '
+      permission_input = gets.chomp.downcase
+    else 
+      print 'Specialization: '
+      specialization = gets.chomp
+    end
+    {user_choice: user_choice, age: age, name: name, permission_input: permission_input, specialization: specialization  }
+  end
+
+  def create_person
+    person_details = get_person_details
+    case person_details[:user_choice].to_i
+    when 1 then create_student(person_details[:age],person_details[:name],person_details[:permission_input])
+    when 2 then create_teacher(person_details[:specialization],person_details[:age],person_details[:name])
     else p 'Please enter either 1 or 2'
     end
   end
 
-  def create_student
-    print 'Has parent permission? [Y/N]: '
-    permission_input = gets.chomp.downcase
+  def create_student(age, name, permission_input)
     permission = permission_input == 'y'
-    @state[:people_list] << Student.new(@age, @name, parent_permission: permission)
+    @state[:people_list] << Student.new(age, name, parent_permission: permission)
     p 'Student created successfully'
     puts ''
   end
 
-  def create_teacher
-    print 'Specialization: '
-    specialization = gets.chomp
-    @state[:people_list] << Teacher.new(specialization, @age, @name)
+  def create_teacher(specialization, age, name)
+    @state[:people_list] << Teacher.new(specialization, age, name)
     p 'Teacher created successfully'
     puts ''
   end
