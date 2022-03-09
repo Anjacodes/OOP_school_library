2464877851eb4486a3b070189068bd68c4afb0df
Anjacodes <anja_schmidt7@posteo.de>
2022-03-09 09:07:53 -0600

    Add Tests For Book, Person and Student Class

---
+++ spec/student_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../student'
+require_relative '../person'
+
+describe Student do
+
+  before :each do
+    @student = Student.new(13, "Harry")
+  end
+
+  it "creates an instance of Student" do
+    expect(@student).to be_a Student
+  end
+
+  it "creates an instance of Person" do
+    expect(@student).to be_a Person
+  end
+
+  it "allows student to play hooky" do
+    expect(@student.play_hooky).to eq "¯\(ツ)/¯"
+  end
+end
